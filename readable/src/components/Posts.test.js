857e2eb6464b436e570638f18ec4e3bd9016e91b
Valter <bloodsportjr@gmail.com>
2017-12-07 13:04:15 -0300

    Add more tests

--- readable/src/components/Posts.test.js
+++ readable/src/components/Posts.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { spy } from 'sinon';
+import { Card } from 'material-ui/Card';
 import Posts from './Posts';
 
 describe('Posts Component', () => {
@@ -7,5 +9,27 @@ describe('Posts Component', () => {
 
     it('renders Posts components correctly', () => {
         expect(posts).toMatchSnapshot();
+    });
+
+    it('renders items correctly when posts has one item', () => {
+        const props = { posts: [{ id: 1, title: 'foo' }]};
+        const list = shallow(<Posts {...props} />);
+        expect(list.find(Card).length).toBe(1);
+    });
+
+    it('renders items correctly when posts are empty', () => {
+        const props = { posts: []};
+        const list = shallow(<Posts {...props} />);
+        expect(list.find(Card).length).toBe(0);
+    });
+
+    it('calls componentDidMount() lifecycle method once', () => {
+        const postsDidMountSpy = spy(Posts.prototype, 'componentDidMount');
+        const dispatch = jest.fn();
+        const wrapper = mount(<Posts dispatch={dispatch} />);
+
+        expect(Posts.prototype.componentDidMount.calledOnce).toBe(true);
+
+        postsDidMountSpy.restore();
     });
 });
